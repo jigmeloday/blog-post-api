eef6afdc418cc73fb314de172eda5419fd0be192
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-10 15:03:08 +0600

    article comment resolved

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :following_count, :follower_count, :article_count, :gender, :username
+  attributes(
+    :id,
+    :name,
+    :email,
+    :following_count,
+    :follower_count,
+    :article_count,
+    :gender,
+    :username
+  )
 end
