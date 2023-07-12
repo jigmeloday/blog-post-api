0ac6efc1b25d5d7280c27b2b4e013a1ec371f4c6
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-12 23:30:39 +0600

    refactor

--- app/services/like_service.rb
+++ app/services/like_service.rb
@@ -5,7 +5,7 @@ class LikeService
   end
 
   def destroy(id, params, current_user)
-    like = Like.find_by(likable_id: id, user_id: current_user.id, likable_type: params[:likable_type])
-    Like.destroy(like.id)
+    like = Like.find_by!(likable_id: id, user_id: current_user.id, likable_type: params[:likable_type])
+    Like.destroy!(like.id)
   end
 end
