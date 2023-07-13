4f49721fba44e0af04dd47dab731b540adf27be0
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-13 23:42:06 +0600

    like count checker

--- app/services/follow_service.rb
+++ app/services/follow_service.rb
@@ -3,4 +3,9 @@ class FollowService
     params[:user_id] = user.id
     Follow.create!(params)
   end
+
+  def destroy(id, user)
+    follow = Follow.find_by!(followed_user_id: id, user_id: user.id)
+    follow.destroy!
+  end
 end
--- spec/requests/follows/destroy.rb
+++ spec/requests/follows/destroy.rb
@@ -8,6 +8,30 @@ describe 'Follow' do
   let!(:user1) { create(:user) }
 
   context 'Success' do
+    let!(:follow) { create(:follow, followed_user: user1, user: user) }
+
+    let!(:params) do
+      {
+        follow: {
+          followed_user_id: user1.id,
+          user: user.id
+        }
+      }
+    end
+
+    it 'follow user' do
+      sign_in(user)
+      expect(Follow.count).to eq(1)
+      delete api_v1_follow_path(user1.id), params: params
+      expect(status).to eq(200)
+      expect(Follow.count).to eq(0)
+      expect(json[:message]).to eq(true)
+      user1.reload
+      expect(user1.follower_count).to eq(0)
+    end
+  end
+
+  context 'Failure (With Validation Errors)' do
     let!(:params) do
       {
         follow: {
@@ -20,16 +44,13 @@ describe 'Follow' do
     it 'follow user' do
       sign_in(user)
       expect(Follow.count).to eq(0)
-      post api_v1_follows_path, params: params
-      expect(status).to eq(200)
-      expect(Follow.count).to eq(1)
-      expect(json[:message]).to eq(true)
-      user1.reload
-      expect(user1.follower_count).to eq(1)
+      delete api_v1_follow_path(user1.id), params: params
+      expect(status).to eq(404)
+      expect(Follow.count).to eq(0)
     end
   end
 
-  context 'Failure (With Validation Errors)' do
+  context 'Failure (Without Signing in)' do
     let!(:follow) { create(:follow, followed_user: user1, user: user) }
     let!(:params) do
       {
@@ -41,29 +62,10 @@ describe 'Follow' do
     end
 
     it 'follow user' do
-      sign_in(user)
       expect(Follow.count).to eq(1)
-      post api_v1_follows_path, params: params
-      expect(status).to eq(422)
-      expect(Follow.count).to eq(1)
-    end
-  end
-
-  context 'Failure (Without Signing in)' do
-    let!(:params) do
-      {
-        follow: {
-          followed_user_id: user1.id,
-          user: user.id
-        }
-      }
-    end
-
-    it 'follow user' do
-      expect(Follow.count).to eq(0)
-      post api_v1_articles_path, params: params
+      delete api_v1_follow_path(user1.id), params: params
       expect(status).to eq(401)
-      expect(Follow.count).to eq(0)
+      expect(Follow.count).to eq(1)
       expect(json[:error]).to eq('You need to sign in or sign up before continuing.')
     end
   end
