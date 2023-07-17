d38cda3fabdc587db226cc627870a1e9fa7c1667
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-18 00:03:00 +0600

    refactor some spec and removed unnessary codes
    Co-authored-by: Dorji Gyeltshen <dg.dorjigyeltshen@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::API
   include ApiErrors::ErrorHandler
   include ParamsSanitizer
-  include Pundit::Authorization
 
   before_action :authenticate_user!
 end
