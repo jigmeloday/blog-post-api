f3059a2819520ab13c446b718fe24f6475fbdc31
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-17 23:39:08 +0600

    comment spec

--- spec/requests/comments/create_spec.rb
+++ spec/requests/comments/create_spec.rb
@@ -52,62 +52,27 @@ describe 'Comment' do
     end
   end
 
-  context 'Failure (with )' do
+  context 'Failure (Without Signing in)' do
     let!(:params) do
       {
-        follow: {
-          followed_user_id: user1.id,
-          user: user1.id
+        comment: {
+          commentable_id: article.id,
+          commentable_type: 'Article',
+          body: 'Hello',
+          user_id: user.id
         }
       }
     end
 
-    it 'is invalid if a user tries to follow themselves' do
-      sign_in(user1)
-      expect(Follow.count).to eq(0)
-      post api_v1_follows_path, params: params
-      expect(response.status).to eq(422)
-      expect(Follow.count).to eq(0)
-      user1.reload
-      expect(user1.follower_count).to eq(0)
+    it 'without user' do
+      expect(Comment.count).to eq(0)
+      post api_v1_comments_path, params: params
+      expect(status).to eq(401)
+      expect(Comment.count).to eq(0)
+      expect(json[:error]).to eq('You need to sign in or sign up before continuing.')
+      article.reload
+      expect(article.comment_count).to eq(0)
     end
   end
-  #
-  # context 'Failure (With invalid params)' do
-  #   let!(:params) do
-  #     {
-  #       follow: {
-  #         user: user.id
-  #       }
-  #     }
-  #   end
-  #
-  #   it 'follow user without followed_id' do
-  #     expect(Follow.count).to eq(0)
-  #     post api_v1_follows_path, params: params
-  #     expect(status).to eq(401)
-  #     expect(Follow.count).to eq(0)
-  #     expect(json[:error]).to eq('You need to sign in or sign up before continuing.')
-  #   end
-  # end
-  #
-  # context 'Failure (Without Signing in)' do
-  #   let!(:params) do
-  #     {
-  #       follow: {
-  #         followed_user_id: user1.id,
-  #         user: user.id
-  #       }
-  #     }
-  #   end
-  #
-  #   it 'follow user' do
-  #     expect(Follow.count).to eq(0)
-  #     post api_v1_follows_path, params: params
-  #     expect(status).to eq(401)
-  #     expect(Follow.count).to eq(0)
-  #     expect(json[:error]).to eq('You need to sign in or sign up before continuing.')
-  #   end
-  # end
 end
 # rubocop: enable Metrics/BlockLength
