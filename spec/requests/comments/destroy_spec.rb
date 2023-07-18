b8c58dd428e4cbacb55d6b5b1d000479a929bbe5
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-18 11:22:03 +0600

    destroy commen

--- spec/requests/comments/destroy_spec.rb
+++ spec/requests/comments/destroy_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop: disable  Metrics/BlockLength
 describe 'Comment' do
   let!(:user) { create(:user) }
   let!(:article) { create(:article, user: user) }
@@ -19,48 +20,28 @@ describe 'Comment' do
     end
   end
 
-  # context 'Failure (With Validation Errors)' do
-  #   let!(:params) do
-  #     {
-  #       comment: {
-  #         commentable_id: article.id,
-  #         commentable_type: 'Article',
-  #         user_id: user.id
-  #       }
-  #     }
-  #   end
-  #
-  #   it 'creat comment' do
-  #     sign_in(user)
-  #     expect(Comment.count).to eq(0)
-  #     post api_v1_comments_path, params: params
-  #     expect(status).to eq(422)
-  #     expect(Comment.count).to eq(0)
-  #     article.reload
-  #     expect(article.comment_count).to eq(0)
-  #   end
-  # end
-  #
-  # context 'Failure (Without Signing in)' do
-  #   let!(:params) do
-  #     {
-  #       comment: {
-  #         commentable_id: article.id,
-  #         commentable_type: 'Article',
-  #         body: 'Hello',
-  #         user_id: user.id
-  #       }
-  #     }
-  #   end
-  #
-  #   it 'without user' do
-  #     expect(Comment.count).to eq(0)
-  #     post api_v1_comments_path, params: params
-  #     expect(status).to eq(401)
-  #     expect(Comment.count).to eq(0)
-  #     expect(json[:error]).to eq('You need to sign in or sign up before continuing.')
-  #     article.reload
-  #     expect(article.comment_count).to eq(0)
-  #   end
-  # end
+  context 'Failure (With wrong ID)' do
+    it 'destroy comment with wrong id' do
+      sign_in(user)
+      expect(Comment.count).to eq(1)
+      post api_v1_comments_path(0)
+      expect(status).to eq(400)
+      expect(Comment.count).to eq(1)
+      article.reload
+      expect(article.comment_count).to eq(1)
+    end
+  end
+
+  context 'Failure (Without Signing in)' do
+    it 'without user' do
+      expect(Comment.count).to eq(1)
+      delete api_v1_comment_path(comment.id)
+      expect(status).to eq(401)
+      expect(Comment.count).to eq(1)
+      expect(json[:error]).to eq('You need to sign in or sign up before continuing.')
+      article.reload
+      expect(article.comment_count).to eq(1)
+    end
+  end
 end
+# rubocop: enable  Metrics/BlockLength
