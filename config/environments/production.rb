06c54db96b2f5c645619d7cfe67809c5d73e4233
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-23 22:36:06 +0600

    email template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -84,19 +84,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: ENV.fetch('host') }
+  config.action_mailer.default_url_options = { host: ENV.fetch('HOST') }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: Rails.application.credentials.dig(:smtp, :address),
     port: Rails.application.credentials.dig(:smtp, :port),
-    domain: 'example.com', # TODO: Add this to credentials as well
+    domain: 'jigmeloday.com', # TODO: Add this to credentials as well
     user_name: Rails.application.credentials.dig(:smtp, :email),
     password: Rails.application.credentials.dig(:smtp, :pwd),
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    open_timeout: 5,
-    read_timeout: 5
+    authentication: 'plain'
+    # enable_starttls_auto: true,
   }
 end
 # rubocop:enable Metrics/BlockLength
