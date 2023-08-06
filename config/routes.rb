8e916e0fdd7b9fa07b885c732b4aa2c0d51ba77e
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-06 17:46:28 +0600

    comment resolved

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
       resources :likes, only: %i[create destroy]
       resources :follows, only: %i[create destroy]
       resources :comments, except: %i[show index]
-      resources :user, only: %i[index]
     end
   end
 end
