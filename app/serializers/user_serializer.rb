8f475779108bbee9ec143da6006c2d9d6f026f4d
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-15 00:18:13 +0600

    fix: image

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -8,10 +8,15 @@ class UserSerializer < ActiveModel::Serializer
     :article_count,
     :gender,
     :username,
+    :profile_url,
     :following
   )
 
   def following
     current_user ? current_user.follows.find_by(followed_user_id: object.id).present? : false
+  end
+
+  def profile_url
+    'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
   end
 end
